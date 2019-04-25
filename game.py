7d9af65e7d7f22f82350eb8c514a6d4b398901ef
Thomas Garver <tgarver@wayfair.com>
2019-04-25 17:07:25 -0400

    Small refactor to clean up code for getting moves

--- game.py
+++ game.py
@@ -13,43 +13,35 @@ class Game:
     
     def play(self):
         while not self.game_over:
-            if self.board.is_checkmate():
-                print("Player " + str(self.current_turn) + " wins!")
-                self.game_over = True
-                break
-            elif self.board.is_stalemate():
-                self.game_over = True
-                print("Stalemate!")
-                break
+            move_complete = False
+            if self.current_turn == 1:
+                player = self.player1
             else:
-                move_complete = False
-                while not move_complete:
-                    if self.current_turn == 1:
-                        player = self.player1
-                    else:
-                        player = self.player2
-                    print(self.board)
-                    print("Player " + str(self.current_turn) + "'s turn")
-                    move = player.get_move(self.board)
-                    if (move == "auto_move" or move == ""):
-                        result = self.engine.play(self.board, chess.engine.Limit(time=1.00))
-                        move = result.move
-                    else:
-                        try:
-                            move = chess.Move.from_uci(move)
-                        except:
-                            print("Invalid move input")
-                            continue
-                    if not (move in self.board.legal_moves):
-                        print("Illegal move")
-                    else:
-                        self.board.push(move)
-                        self.current_turn = (self.current_turn % 2) + 1
-                        move_complete = True
+                player = self.player2
+            while not move_complete:
+                info = self.engine.analyse(self.board, chess.engine.Limit(time=1.00))
+                print(info["score"])
+                print(self.board)
+                print("Player " + str(self.current_turn) + "'s turn")
+                move = player.get_move(self.board)
+                if not move or not (move in self.board.legal_moves):
+                    print("Illegal move")
+                else:
+                    self.board.push(move)
+                    if self.board.is_checkmate():
+                        print("Player " + str(self.current_turn) + " wins!")
+                        self.game_over = True
+                        break
+                    elif self.board.is_stalemate():
+                        self.game_over = True
+                        print("Stalemate!")
+                        break
+                    self.current_turn = (self.current_turn % 2) + 1
+                    move_complete = True
         print("Game over")
 
 if __name__ == "__main__":
-    player1 = player.Player("Test1", player.Player.HUMAN)
-    player2 = player.Player("Test2", player.Player.COMPUTER)
+    player1 = player.Player("Test1", player.Player.COMPUTER, engine_time_limit=0.001)
+    player2 = player.Player("Test2", player.Player.COMPUTER, engine_time_limit=0.100)
     game = Game(player1, player2)
     game.play()
--- player.py
+++ player.py
@@ -3,18 +3,25 @@ import chess.engine
 class Player:
     COMPUTER = 1
     HUMAN = 2
-    def __init__(self, name, type):
+    def __init__(self, name, type, engine_time_limit = 1.00):
         self.name = name
         self.type = type
-        if type == self.COMPUTER:
-            self.engine = chess.engine.SimpleEngine.popen_uci("/usr/local/bin/stockfish")
-            self.engine_time_limit = 1.00
+        self.engine = chess.engine.SimpleEngine.popen_uci("/usr/local/bin/stockfish")
+        self.engine_time_limit = engine_time_limit
 
     def get_move(self, board):
         if self.type == self.COMPUTER:
             result = self.engine.play(board, chess.engine.Limit(time=self.engine_time_limit))
             print("Getting move from computer player\n")
-            return result.move.uci()
+            return result.move
         else:
             move = input("Enter your move:\n")
+            if (move == "auto_move" or move == ""):
+                result = self.engine.play(board, chess.engine.Limit(time=1.00))
+                move = result.move
+            else:
+                try:
+                    move = chess.Move.from_uci(move)
+                except:
+                    return None
             return move
